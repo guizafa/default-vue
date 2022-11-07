828670a9bd5f6ef57360693c670de40b40203c60
guizafa <guizafandroid@gmail.com>
2022-11-07 11:25:27 -0300

    feature: Setup Vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PanelMenu",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  //created: function () {}, Code to Start With Component
+  //mounted() {}, Code Start When Component is Ready
+  //unmounted() {} Code Start When Component is Closing
+});
+</script>
+<style></style>
---
+++ src/components/menu/menu-app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ct-menu-app flex"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MenuApp",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  //created: function () {}, Code to Start With Component
+  //mounted() {}, Code Start When Component is Ready
+  //unmounted() {} Code Start When Component is Closing
+});
+</script>
+<style></style>
